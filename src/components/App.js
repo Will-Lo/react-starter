235fd957caa49a9a720f642c0dfa09ba6f06bdac
William Lo <will-i-am_95@hotmail.com>
2016-08-29 09:59:52 -0400

    Initial config

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,9 @@ import '../stylesheets/App.css';
 const Title = React.createClass({
   render() {
     return (
+      <div>
         <h1 className='header'>Welcome to React</h1>
+      </div>
     );
   }
 });
