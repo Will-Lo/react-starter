74ac301bc660c3d01f8592cf0dc145c7aec8aa6b
William Lo <will-i-am_95@hotmail.com>
2017-03-02 22:20:05 -0500

    Added build script and files and re-added bootstrap due to popular demand

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
 const webpack = require('webpack');
 const path = require('path');
+const DEBUG = (process.argv.indexOf('--release') === -1);
 
 module.exports = {
   entry: [
-    'webpack-dev-server/client?http://localhost:8080',
+    'webpack-dev-server/client?http://localhost:3000',
     './src/containers/main.js',
     './src/stylesheets/main.scss'
   ],
   output: {
+    path: path.resolve(__dirname, "build"),
     publicPath: '/public/',
     filename: 'bundle.js'
   },
@@ -31,5 +33,12 @@ module.exports = {
         loader: 'file'
       },
     ]
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+         NODE_ENV: JSON.stringify(DEBUG ? "development" : "production")
+       }
+    })
+  ]
 }
